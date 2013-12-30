8cc5ce12fb96eef6d3e78e5420288853fd310f3a
LumenTeun <teun@lutic.org>
2013-12-30 22:49:55 +0100

    Changed how content is centered, again.

--- violet.js
+++ violet.js
@@ -1,5 +1,5 @@
 function repositionContent(){
-    $('#content').css({"top": window.innerHeight/2 - $('#content').height() / 2});
+    $('#content').css({"top": window.innerHeight/2 - $('#info').height() / 2 - $('h1').height()});
 }
 
 $(document).ready(function(){
