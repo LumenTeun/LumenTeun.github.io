799bc097be78b15383f938025d8550bb52b75916
LumenTeun <teun@lutic.org>
2014-06-17 15:57:03 +0100

    Added #top height JS and commented out unused code for now.

--- violet.js
+++ violet.js
@@ -1,5 +1,6 @@
 function repositionContent(){
-    $('#content').css({"top": window.innerHeight/2 - $('#info').height() / 2 - $('h1').height()});
+    //$('#content').css({"top": window.innerHeight/2 - $('#info').height() / 2 - $('h1').height()});
+    $('#top').css({ "height": window.innerHeight/2 });
 }
 
 $(document).ready(function(){
