a42027b80acfb1e26c72fd23aa456c9c7b7d20ac
LumenTeun <teun@lutic.org>
2014-06-18 15:29:17 +0100

    Added and positioned images.

--- violet.js
+++ violet.js
@@ -1,6 +1,8 @@
 function repositionContent(){
     //$('#content').css({"top": window.innerHeight/2 - $('#info').height() / 2 - $('h1').height()});
     $('#top').css({ "height": window.innerHeight/2 });
+    $('#me').css({ "height": window.innerHeight/3, "margin-top": window.innerHeight/12 });
+    $('#info').css({ "margin-top": window.innerHeight/12 });
 }
 
 $(document).ready(function(){
