cdc679ed2e4cd8512b47eb56475d525dd7437390
LumenTeun <teun@lutic.org>
2014-06-26 13:25:51 +0100

    Repositioning stuff. Probably need adjusting.

--- violet.js
+++ violet.js
@@ -1,8 +1,7 @@
 function repositionContent(){
-    //$('#content').css({"top": window.innerHeight/2 - $('#info').height() / 2 - $('h1').height()});
-    $('#top').css({ "height": window.innerHeight/2 });
-    $('#me').css({ "height": window.innerHeight/3, "margin-top": window.innerHeight/12 });
-    $('#info').css({ "margin-top": window.innerHeight/12 });
+    $('#top').css({ "height": window.innerHeight/2.5});
+    $('#me').css({ "height": window.innerHeight/3, "top": window.innerHeight/24 });
+    $('#info').css({ "top": window.innerHeight/5 });
 }
 
 $(document).ready(function(){
