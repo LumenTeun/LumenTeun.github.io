338c99cf0b2109176a473c512418f72dde5d6009
Anton Strömkvist <hi@ahst.ro>
2017-08-12 12:51:10 +0200

    Add 'link' function to Helpers

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -1,9 +1,17 @@
 module Helpers exposing (..)
 
-import Html exposing (Html, div, text)
+import Html exposing (Html, div, text, a)
+import Html.Attributes exposing (href)
 import Update exposing (Msg(..))
 
 
 line : List (Html Msg) -> Html Msg
 line =
     div []
+
+
+link : String -> String -> Html Msg
+link url txt =
+    a [ href url ]
+        [ text txt
+        ]
