0d3f3853db894d2d69f3715d6210e741287e6454
Anton Strömkvist <anton@stromkvist.com>
2019-03-22 01:28:07 +0100

    Randomly set theme on start

---
+++ randomTheme.js
@@ -0,0 +1,11 @@
+const THEMES = ["salmon", "lime", "dust"];
+
+function setThemeRandomly() {
+  const oldClassName = document.body.className;
+  document.body.className = THEMES[Math.floor(Math.random() * THEMES.length)];
+  if (oldClassName == document.body.className) {
+    setThemeRandomly();
+  }
+}
+
+setThemeRandomly();
